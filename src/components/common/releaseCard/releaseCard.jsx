75c5e6fb3494577b4d699c7c431a3cd2f1ffc333
themosesnyambu <nyamosekitando@gmail.com>
2021-07-23 21:44:06 +0300

    card for each release

---
+++ src/components/common/releaseCard/releaseCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./releaseCard.scss";
+
+const ReleaseCard = ({ collectionName, itemName, price, thumbnail }) => (
+  <>
+    <Link className="article-section" to="*">
+      <div className="release-card">
+        <img src={thumbnail} alt="release cover" />
+        <div className="details">
+          <h3>{collectionName}</h3>
+          <h3>{itemName}</h3>
+          <h4>{price}</h4>
+        </div>
+      </div>
+    </Link>
+  </>
+);
+
+ReleaseCard.propTypes = {
+  thumbnail: PropTypes.string.isRequired,
+  itemName: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  collectionName: PropTypes.number.isRequired,
+};
+
+export default ReleaseCard;
