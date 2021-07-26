18360fd37974f2a8ecc7dab5f1d5f8e481308d46
themosesnyambu <nyamosekitando@gmail.com>
2021-07-26 08:15:40 +0300

    toastr config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Toastr from "toastr";
+import App from "./App";
+import "../node_modules/toastr/build/toastr.css";
+
+Toastr.options = {
+  closeButton: true,
+  debug: false,
+  newestOnTop: false,
+  progressBar: false,
+  positionClass: "toast-top-center",
+  preventDuplicates: false,
+  onclick: null,
+  showDuration: "5000",
+  hideDuration: "3000",
+  timeOut: "5000",
+  extendedTimeOut: "5000",
+  showEasing: "swing",
+  hideEasing: "linear",
+  showMethod: "fadeIn",
+  hideMethod: "fadeOut",
+};
 
 ReactDOM.render(
   // eslint-disable-next-line react/jsx-filename-extension
   <App />,
-  document.getElementById('app')
+  document.getElementById("app")
 );
