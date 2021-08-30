fd96ea9bf39653ac20b8d2f74c4daf77071ae650
themosesnyambu <nyamosekitando@gmail.com>
2021-08-30 20:08:38 +0300

    user profile component

--- src/components/views/profile/profile.jsx
+++ src/components/views/profile/profile.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { fetchUserProfile } from "../../../redux/actions/profileActions";
 
-const Profile =()=> <div/>
+const Profile = ({ match }) => {
+  const user = useSelector((state) => state.auth.user);
+//   const profile = useSelector((state) => state.profile);
 
+  useEffect(() => {
+    const getProfile = async () => {
+      const { params } = match;
+      await fetchUserProfile(params.userId);
+    };
+    getProfile();
+  }, [match, fetchUserProfile, user]);
+
+  return <></>;
+};
+
+Profile.propTypes = {
+  profile: PropTypes.shape({}).isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      userId: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+  user: PropTypes.shape({}).isRequired,
+};
 
 export default Profile;
